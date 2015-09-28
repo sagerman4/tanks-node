e59a968ffe6cf655f8059d31157cbe8765045826
sagerman4 <sager.davidson@gmail.com>
2015-09-28 14:24:08 -0500

    Added some endpoint thingys.

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 server.connection({port: 3000});
 
+server.route({
+  method: 'GET',
+  path: '/tanks',
+  handler: function (request, reply) {
+    reply('some tanks!');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/tanks/{id}',
+  handler: function (request, reply) {
+    reply(request.params.id + ' tanks!');
+  }
+});
+
+
 server.start(function() {
   console.log('Server runnin at: ', server.info.uri);
 });
