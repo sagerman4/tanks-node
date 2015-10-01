66be66107a78bc143f6f68977f0bc424a5ab7e83
Sager Davidson <sager.davidson@gmail.com>
2015-10-01 18:55:34 +0000

    Removed node_modules directory.  That was dumb. Oracle query working.  Added a little helper function. Added Inert package for Hapi, since it won't use the directory provider without it.

--- server.js
+++ server.js
@@ -1,25 +1,57 @@
 var Hapi = require('hapi');
+var Inert = require('inert');
+var oracledb = require('oracledb');
+
+var connectionConfig = {user: "TANKS_APP",password: "oracle",connectString: "10.0.2.2:1521/XE"};
+
+var query = function(sql, callback) {
+  oracledb.getConnection(connectionConfig, function(err, connection) {
+    if(err) { console.error(err.message); return; }
+    connection.execute(sql,{},{maxRows: 150000},function(err,result){
+      if(err) {console.error(err.message); return; }
+      callback(err, result.rows);
+    });
+  });
+};
 
 var server = new Hapi.Server();
-server.connection({port: 3000});
 
-server.route({
-  method: 'GET',
-  path: '/tanks',
-  handler: function (request, reply) {
-    reply('some tanks!');
+server.register(Inert, function () {
+  server.connection({ port: 3000 });
+  server.route({
+    method: 'GET',
+    path: '/tanks',
+    handler: function (request, reply) {
+      var callback = function(err, rows){
+        if(err) {reply(err); return;}
+        reply(rows);
+        return;
+      };  
+      query("select * from tanks", callback);
   }
+ });
+
+ server.route({
+   method: 'GET',
+   path: '/tanks/{id}',
+   handler: function (request, reply) {
+     reply(request.params.id + ' tanks!');
+   }
+ });
+ 
+ server.route({
+     method: 'GET',
+     path: '/{param*}',
+     handler: {
+         directory: {
+             path: '.'
+         }
+     }
+ });
+ 
+ server.start(function() {
+   console.log('Server runnin at: ', server.info.uri);
+ });
 });
 
-server.route({
-  method: 'GET',
-  path: '/tanks/{id}',
-  handler: function (request, reply) {
-    reply(request.params.id + ' tanks!');
-  }
-});
 
-
-server.start(function() {
-  console.log('Server runnin at: ', server.info.uri);
-});
